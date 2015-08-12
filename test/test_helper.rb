f47ea482518b661e724bc9b62201f4a68a1af02e
Lucas Ocon <lucas_masterclas@hotmail.com>
2015-08-12 12:23:40 -0300

    Add tests

---
+++ test/test_helper.rb
@@ -0,0 +1,10 @@
+require './lib/openweather'
+require 'minitest/autorun'
+require 'webmock/minitest'
+require 'vcr'
+
+VCR.configure do |c|
+  c.cassette_library_dir = "test/fixtures"
+  c.hook_into :webmock
+end
+
