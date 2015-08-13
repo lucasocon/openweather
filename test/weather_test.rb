826a136e057c24643c06877bc6176bc5d328dc3f
Lucas Ocon <lucas_masterclas@hotmail.com>
2015-08-13 17:58:28 -0300

    -Remove bad test

--- test/weather_test.rb
+++ test/weather_test.rb
@@ -13,13 +13,4 @@ class WeatherTest < Minitest::Test
     assert response.longitude == -0.13
   end
 
-  def test_fail_weather_for_city
-    stub_request(:get, "http://api.openweathermap.org/data/2.5/weather?q=London&APPID=dd7073d18e3085d0300b6678615d904d").to_return(body: fixture('london.json'), headers: {content_type: 'application/json; charset=utf-8'})
-
-    response = Openweather2.weather('london')
-    assert response.city != 'Manchester'
-    assert response.latitude != 53.48
-    assert response.longitude != -2.24
-  end
-
 end
