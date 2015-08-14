7bb4c08103d0722a4abef4345b39291c353ac622
Lucas Ocon <lucas_masterclas@hotmail.com>
2015-08-14 16:50:28 -0300

    Refactor code

--- lib/openweather2/client.rb
+++ lib/openweather2/client.rb
@@ -26,29 +26,33 @@ module Openweather2
 
   def weather(city, units = nil)
     configure_required!
-
     uri = URI(Openweather2.configuration.endpoint)
     uri.query = URI.encode_www_form(default_params.merge(:q => city, :units => units))
     req = Net::HTTP::Get.new(uri.request_uri)
+    res = get_request(uri, req)
+    get_response(res)
+  end
 
+  private
+
+  def get_request(uri, req)
     http_params = [uri.hostname, uri.port, use_ssl: uri.scheme == 'https']
     res = Net::HTTP.start(*http_params) do |http|
       http.request(req)
     end
-
+  end
+  
+  def get_response(res)
     case res
     when Net::HTTPSuccess
       json = JSON.parse(res.body)
       Openweather2::Weather.new(json)
-
     when Net::HTTPUnprocessableEntity
       raise UnprocessableError, 'Bad URI param!'
     else
       raise UnknownResponse, 'Something was wrong!'
     end
   end
-
-  private
 
   def configure_required!
     if Openweather2.configuration.instance_variables.size < 2
