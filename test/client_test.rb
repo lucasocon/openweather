200347c89d3b3f8ce6762c8b6bfc20841abc7b6a
Lucas Ocon <lucas_masterclas@hotmail.com>
2015-08-18 22:20:35 -0300

    new tests

--- test/client_test.rb
+++ test/client_test.rb
@@ -2,11 +2,25 @@ require './test/test_helper'
 
 class ClientTest < Minitest::Test
 
-  def test_get_weather_client
+  def test_get_weather_by_city
     configure_openweather2
     stub_request(:get, "http://api.openweathermap.org/data/2.5/weather?q=London&APPID=dd7073d18e3085d0300b6678615d904d")
     response = Openweather2.get_weather(city:'london')
     assert_equal response.city, 'London'
+  end
+
+  def test_get_weather_by_coordinates
+    configure_openweather2
+    stub_request(:get, "http://api.openweathermap.org/data/2.5/weather?q=London&APPID=dd7073d18e3085d0300b6678615d904d")
+    response = Openweather2.get_weather(lat: 35, lon: 139)
+    assert_equal response.city, 'Shuzenji'
+  end
+
+  def test_get_weather_by_zip
+    configure_openweather2
+    stub_request(:get, "http://api.openweathermap.org/data/2.5/weather?q=London&APPID=dd7073d18e3085d0300b6678615d904d")
+    response = Openweather2.get_weather(zip: 94040)
+    assert_equal response.city, 'Mountain View'
   end
 
   def test_get_nil_response
